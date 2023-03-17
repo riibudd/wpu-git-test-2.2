ee7498dbc35d598b9c6248881947377c07415c16
riibudd <webrizky08@gmail.com>
2023-03-17 10:59:37 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
   <h1>Hello World !</h1>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos quis deserunt aspernatur, ullam sequi optio hic velit nesciunt nihil atque!</p>
 </body>
 </html>
